88565eae8388f6b62b497ed2abaa51bbe3ab6366
Adam Stankiewicz <sheerun@sher.pl>
2018-09-27 17:15:14 +0200

    reproduce

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import App, { Container } from 'next/app'
+
+class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    throw new Error('hello world')
+
+    return { pageProps, asPath: router.asPath }
+  }
+
+  render () {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
+}
+
+export default MyApp
